f800b684cb8f0707df4a877f4867fbdbb4eecfa0
LethalPants <48921721+LethalPants@users.noreply.github.com>
2020-04-27 04:20:53 +0300

    Reconfigure commit history

---
+++ app.py
@@ -0,0 +1,59 @@
+import io
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, render_template, request, jsonify
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import imagenet_utils
+from tensorflow.keras.preprocessing.image import img_to_array
+
+app = Flask(__name__)
+model = None
+
+
+def load_model():
+    # load the Resenet model pre-trained on the imagenet dataset
+    global model
+    model = ResNet50(weights="imagenet")
+
+
+def prepare_image(image, target):
+    # if the image mode is not RGB, convert it
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    image = image.resize(target)
+    image = img_to_array(image)
+    image = tf.expand_dims(image, axis=0)
+    image = imagenet_utils.preprocess_input(image)
+    return image
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # initialize the data dictionary that will be returned from the view
+    data = {"success": False}
+    if request.method == "POST":
+        if request.files["image"]:
+            image = request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+            image = prepare_image(image, target=(224, 224))
+            prediction = model.predict(image)
+            results = imagenet_utils.decode_predictions(prediction)
+            data["predictions"] = []
+
+            for (imagenetID, label, prob) in results[0]:
+                r = {"label": label, "probability": float(prob)}
+                data["predictions"].append(r)
+
+            data["success"] = True
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    if model is None:
+        load_model()
+    app.run()
---
+++ static/script.js
@@ -0,0 +1,30 @@
+let errorContainer = document.getElementById('error-container');
+let output = document.getElementById("output");
+document.getElementById("image").addEventListener("change", ev => {
+    let imageBox = document.getElementById('image-box');
+    let image = ev.target.files[0];
+    imageBox.innerHTML = `<img src=${URL.createObjectURL(image)} alt="picture">`;
+    const formData = new FormData();
+    formData.append('image', image);
+
+    fetch("/predict", {
+        method: 'POST',
+        body: formData
+    }).then(res => res.json()).then(data => makeSentence(data)).catch(err => {
+        errorContainer.innerHTML = `<div class="error"> Something went wrong. Please reload the page and try again. </div>`;
+        console.log(err)
+    })
+});
+
+
+function makeSentence(data) {
+    if (data.success === true) {
+        let predLabel = data.predictions[0].label;
+        let prob = Math.round(data.predictions[0].probability * 100);
+        predLabel = predLabel.replace(/_/g, ' ');
+        output.innerHTML = `<h3> I'm ${prob}% sure that is a ${predLabel}</h3>`
+    } else {
+        errorContainer
+            .innerHTML = `<div class="error"> Something went wrong. Please reload the page and try again. </div>`
+    }
+}
